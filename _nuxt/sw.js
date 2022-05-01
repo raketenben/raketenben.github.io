858a3ded01db4a80ee8b9b92c51497bf8f925140
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2022-05-01 18:35:42 +0200

    survey app

---
+++ _nuxt/sw.js
@@ -0,0 +1,14 @@
+//install
+self.addEventListener('install', function(event) {
+
+});
+
+//activate
+self.addEventListener('activate', function(event) {
+
+});
+
+//fetch
+self.addEventListener('fetch', function(event) {
+
+});
